5f57dc6900df33a2d622b1e43d3fde13ef92157e
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-19 22:38:52 +0300

    import list_books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from . import views
+from views import list_books
 
 urlpatterns = [
     path('books/', views.index, name='list_books'),
