a430051e14a525dd60c77215ab73b2d2e6240745
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-19 22:57:23 +0300

    auth views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,37 @@ from django.http import HttpResponse
 from .models import Author, Book, Librarian
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponse("Signup successful!")
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Login successful!")
+        else:
+            return HttpResponse("Invalid login credentials.")
+    return render(request, 'login.html')
+def logout(request):
+    pass
+
+
+
 def index(request): 
     books = Book.objects.all()
     context = {
