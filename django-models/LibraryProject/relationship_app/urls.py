01b2601c3a979b35e6a5b129698800ef6c11e7bf
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-19 22:59:16 +0300

    vuew urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,5 +21,8 @@ from .views import list_books
 urlpatterns = [
     path('books/', views.index, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     
 ]
