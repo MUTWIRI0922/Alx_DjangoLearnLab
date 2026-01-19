80cc78c34f04299525fabf9a61bdef88f033d26d
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-19 23:42:19 +0300

    task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from . import views
 from .views import list_books
+from .admin_view import admin_dashboard
+from .librarian_view import librarian_dashboard
+from .member_view import member_dashboard
+
 
 urlpatterns = [
     path('books/', views.index, name='list_books'),
@@ -24,5 +28,9 @@ urlpatterns = [
     path('signup/', views.register, name='signup'),
     path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
+    path('member-dashboard/', member_dashboard, name='member_dashboard'),
+
     
 ]
