a978fb0295af6925ad68e2809d71ecbb51ccef81
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-19 22:30:50 +0300

    views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('books/', views.index, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     
-
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.http import HttpResponse
+from .models import Author, Book, Library, Librarian
 
 # Create your views here.
+def index(request): 
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'list_books.html', context)
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['librarian'] = self.object.librarian
+        context['books'] = self.object.books.all()
+        return context
