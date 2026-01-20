11213e2a736573921935c6dd2758415874528e8c
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-20 10:33:01 +0300

    last

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,15 +18,15 @@ def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
-def admin_dashboard(request):
+def Admin(request):
     return render(request, 'admin_view.html')
 
 @user_passes_test(is_librarian)
-def librarian_dashboard(request):
+def Librarian(request):
     return render(request, 'librarian_view.html')
 
 @user_passes_test(is_member)
-def member_dashboard(request):
+def Member(request):
     return render(request, 'member_view.html')
 
 @permission_required('relationship_app.can_add_book', raise_exception=True)
