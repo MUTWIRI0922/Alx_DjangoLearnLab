1fdfdd868c50b392329d477b429b3224f2a9fdc8
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-20 09:08:54 +0300

    @permision decorator

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Author, Book, Librarian
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -21,6 +22,39 @@ def Librarian(request):
 
 def Member(request):
     return HttpResponse("Member View")
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            published_date=published_date
+        )
+        return redirect('book_list')
+
+    return render(request, 'add_book.html')
+@permission_required(relationship_app.can_change_book)
+def change_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.published_date = request.POST.get('published_date')
+        book.save()
+        return redirect('book_list')
+
+    return render(request, 'change_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete()
+    return HttpResponse("Delete Book View")
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
