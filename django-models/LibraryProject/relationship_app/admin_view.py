f02a20b08a370039e18a5a2cba4c29bd4fe32e9c
MUTWIRI0922 <amutwiri07@gmail.com>
2026-01-20 08:19:46 +0300

    admin view

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+class Admin(request):
 
-@user_passes_test(is_admin)
-def admin_dashboard(request):
-    return render(request, 'admin_view.html')
+    def is_admin(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+    @user_passes_test(is_admin)
+    def admin_dashboard(request):
+        return render(request, 'admin_view.html')
