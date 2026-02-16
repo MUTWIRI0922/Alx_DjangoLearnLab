cda48c57e2e2230cf54c481682613015669a7f92
MUTWIRI0922 <amutwiri07@gmail.com>
2026-02-16 23:10:08 +0300

    widgets

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
+from taggit.forms import TagWidget
+
 
 
 class RegisterForm(UserCreationForm):
@@ -25,6 +27,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
     def save(self, commit=True, user=None):
         post = super().save(commit=False)
