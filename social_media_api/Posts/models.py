2d5486690803730bcbb3318f7943e4c11dfe9e43
MUTWIRI0922 <amutwiri07@gmail.com>
2026-02-19 10:52:56 +0300

    posts app

--- social_media_api/Posts/models.py
+++ social_media_api/Posts/models.py
@@ -20,3 +20,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f'{self.user.username} likes {self.post.title}'
