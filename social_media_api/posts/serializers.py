755291b2600dfba304bad719d6d63e4745cf434f
MUTWIRI0922 <amutwiri07@gmail.com>
2026-02-20 08:54:26 +0300

    posts

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = ['author', 'created_at', 'updated_at']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ['author', 'created_at', 'updated_at']
